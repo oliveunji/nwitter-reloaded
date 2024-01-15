e8bf6b628ca7885a6b1d44bb55fdd4e5501d044c
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2024-01-15 21:55:53 +0900

    feat: implement realtime tweets

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -35,10 +35,9 @@ export default function Tweet({ username, photo, tweet }: ITweet) {
                 <Username>{username}</Username>
                 <Payload>{tweet}</Payload>
             </Column>
-            {photo ? (
-                <Column>
+            <Column>
+                {photo ? (
                     <Photo src={photo} />
-                </Column>
-            ) : null}
+                ) : null}</Column>
         </Wrapper>)
 }
