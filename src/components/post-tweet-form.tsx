0c0bb8203ebd3a61037739d8dd18e192ea1dfb92
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2024-01-15 21:02:53 +0900

    feat: add uploading images feature

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,8 @@
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import { styled } from "styled-components"
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
     display: flex;
@@ -69,9 +72,38 @@ export default function PostTweetForm() {
             setFile(files[0]);
         }
     }
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user = auth.currentUser
+        if (!user || isLoading || tweet === "" || tweet.length > 180) return;
 
-    return <Form>
-        <TextArea rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?" />
+        try {
+            setLoading(true)
+            const doc = await addDoc(collection(db, "tweets"), {
+                tweet,
+                createdAt: Date.now(),
+                username: user.displayName || "Anonymous",
+                userId: user.uid,
+            })
+            if (file) {
+                const locationRef = ref(storage, `tweets/${user.uid}-${user.displayName}/${doc.id}`)
+                const result = await uploadBytes(locationRef, file);
+                const url = await getDownloadURL(result.ref)
+                await updateDoc(doc, {
+                    photo: url
+                })
+            }
+            setTweet("")
+            setFile(null)
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return <Form onSubmit={onSubmit}>
+        <TextArea required rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?" />
         <AttachFileButton htmlFor="file">{file ? "Photo added" : "Add photo"}</AttachFileButton>
         <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*" />
         <SubmitBtn type="submit" value={isLoading ? "Posting..." : "Post tweet"} />
